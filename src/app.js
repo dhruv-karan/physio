493ac2a652ac56f14bee78bd3bf84acb755097f4
dhruv-karan <dhruvkaran24@gmail.com>
2018-10-19 13:35:59 +0530

    added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 //  Views
 import './views/index.pug'
+import './views/pages/aboutUspage.pug'
+// import './views/pages/acupunture.pug'
+import './views/pages/chiropracter.pug'
+import './views/pages/dietitian.pug'
+// import './views/pages/massage.pug'
+import './views/pages/physiotherapy.pug'
+import './views/pages/allservices.pug'
+import './views/pages/doc1.pug'
+import './views/pages/doc2.pug'
+import './views/pages/doc3.pug'
+import './views/pages/doc4.pug'
+import './views/pages/doc5.pug'
+import './views/pages/doc6.pug'
+import './views/pages/doc7.pug'
+import './views/pages/doc8.pug'
+import './views/pages/doc9.pug'
+import './views/pages/allteam.pug'
+
 
 //  Styles
 import './../node_modules/normalize.css/normalize.css'
 import './styles/app.sass'
+import './styles/pages/aboutUspage.sass'
+import './styles/pages/services.sass'
+import './styles/pages/docs.sass'
 
 // Scripts
 const app = () => console.log('aa')
